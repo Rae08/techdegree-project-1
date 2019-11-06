1f5896634ca69c1d8507359828e2a53a508aa296
Rae <raefeldman@gmail.com>
2019-11-05 21:24:06 -0800

    created printQuote function and added comments

--- js/script.js
+++ js/script.js
@@ -1,68 +1,65 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
+// quotes is an array that holds objects. The objects are quotes with properties quote, source, citation, and year.
 var quotes = [
   {
-    quote: "It does not do to dwell on dreams and forget to live",
-    source: "Albus Dumbledore",
-    citation: "Sorcerer’s Stone"
+    quote: 'It does not do to dwell on dreams and forget to live',
+    source: 'Albus Dumbledore',
+    citation: 'Sorcerer’s Stone'
   },
   {
-    quote: "I am what I am an’ I’m not ashamed",
-    source: "Rubeus Hagrid",
-    citation: "Goblet of Fire",
+    quote: 'I am what I am an’ I’m not ashamed',
+    source: 'Rubeus Hagrid',
+    citation: 'Goblet of Fire',
   },
   {
-    quote: "Always",
-    source: "Severus Snape",
-    citation: "Deathly Hallows",
+    quote: 'Always',
+    source: 'Severus Snape',
+    citation: 'Deathly Hallows',
   },
   {
-    quote: "Differences of habit and language are nothing at all if our aims are identical and our hearts are open",
-    source: "Albus Dumbledore",
-    citation: "Goblet of Fire"
+    quote: 'Differences of habit and language are nothing at all if our aims are identical and our hearts are open',
+    source: 'Albus Dumbledore',
+    citation: 'Goblet of Fire'
   },
   {
-    quote: "Things we lose have a way of coming back to us in the end, if not always in the way we expect",
-    source: "Luna Lovegood",
-    citation: "Order of the Pheonix",
-    year: "2003",
+    quote: 'Things we lose have a way of coming back to us in the end, if not always in the way we expect',
+    source: 'Luna Lovegood',
+    citation: 'Order of the Pheonix',
+    year: '2003',
   }
 ];
 
-
-
-/***
- * `getRandomQuote` function
-***/
-
+// getRandomQuote returns a quote at random from the quotes array. Returns an object.
 function getRandomQuote() {
   var randomNumber = Math.floor(Math.random() * quotes.length);
   randomQuote = quotes[randomNumber];
   return randomQuote;
 };
 
-console.log(getRandomQuote());
+// printQuote uses getRandomQuote to get a random quote object, and then creates html based on the object properties to print the quote to the page
+function printQuote() {
+  selectQuote = getRandomQuote();
+  // var html = '';
+  var html = '<p class="quote">' + selectQuote.quote + '</p>';  
+  html += '<p class="source">' + selectQuote.source;
 
-/***
- * `printQuote` function
-***/
+  // citation is an option property so this checks to see if it exists. 
+  if (selectQuote.citation) {
+    html += '<span class="citation">' + selectQuote.citation + '</span>';
+  }
 
+  // year is an option property so this checks to see if it exists. 
+  if (selectQuote.year) {
+    html += '<span class="citation">' + selectQuote.year + '</span>';
+  }
+  html += '</p>'
+  document.getElementById('quote-box').innerHTML = html;
+}
 
+printQuote();
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE OF CODE BELOW!!
 ***/
 
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener('click', printQuote, false);
