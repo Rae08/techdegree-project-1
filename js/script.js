ac8f711c8582c3b6c2d8e81f9ebb935cac4e4b1a
Rae <raefeldman@gmail.com>
2019-11-05 20:05:28 -0800

    create quotes array

--- js/script.js
+++ js/script.js
@@ -11,6 +11,35 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+var quotes = [
+  {
+    quote: "It does not do to dwell on dreams and forget to live",
+    source: "Albus Dumbledore",
+    citation: "Sorcerer’s Stone"
+  },
+  {
+    quote: "I am what I am an’ I’m not ashamed",
+    source: "Rubeus Hagrid",
+    citation: "Goblet of Fire",
+  },
+  {
+    quote: "Always",
+    source: "Severus Snape",
+    citation: "Deathly Hallows",
+  },
+  {
+    quote: "Differences of habit and language are nothing at all if our aims are identical and our hearts are open",
+    source: "Albus Dumbledore",
+    citation: "Goblet of Fire"
+  },
+  {
+    quote: "Things we lose have a way of coming back to us in the end, if not always in the way we expect",
+    source: "Luna Lovegood",
+    citation: "Order of the Pheonix",
+    year: "2003",
+  }
+];
+
 
 
 /***
